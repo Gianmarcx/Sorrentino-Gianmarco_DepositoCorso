87f6e4787e43069aac6d3021e0745711b413b8bf
Gianmarcx <sorrentinogianmarco179@gmail.com>
2025-11-17 23:53:37 +0100

    organizzazione day 3
    organizzazione day 3

--- corso python/lunedì_17-11/analizzatori_dati_vendita.py
+++ corso python/lunedì_17-11/analizzatori_dati_vendita.py
@@ -1,58 +1,18 @@
-def inserisci_importi():
-    while True:
-        importi_vendita = input("Inserisci gli importi di vendita separati da uno spazio: ")
+def inserisci_importi(): #definiamo la funziona inserisci_importi()
+    while True: #ciclo while infinito fin quando non viene inserito un input giusto
+        importi_vendita = input("Inserisci gli importi di vendita separati da uno spazio: ") #variabile che chiede di inserire in input degli importi di vendita separati da uno spazio
 
-        if not importi_vendita.strip():
+        if not importi_vendita.strip(): #segnala nel caso non siano stati inseriti importi
             print("Non sono stati inseriti importi, riprova.")
-            continue
+            continue #ricomincia il ciclo while
 
-        importi_lista = importi_vendita.split()
+        importi_lista = importi_vendita.split() #divide la stringa in singoli elementi 
 
-        try:
+        try: #prova a convertire gli importi di vendita float in numeri interi 
             importi_interi = [int(float(x)) for x in importi_lista]
             print("Importi convertiti in interi:", importi_interi)
-            return importi_interi  # restituisce la lista e esce dal ciclo
-        except ValueError:
+            return importi_interi  # restituisce la lista e esce dalla funzione
+        except ValueError: #se uno degli elementi non è valido , stampa l'errore
             print("Errore: sono ammessi solo numeri separati da uno spazio, riprova.")
 
 
-def menu_vendite():
-    importi_vendita = []  # Lista iniziale vuota
-
-    while True:
-        print("\n--- Menu Vendite ---")
-        print("1. Inserisci nuove vendite")
-        print("2. Mostra totale e media delle vendite")
-        print("3. Esci")
-
-        try:
-            scelta = int(input("Quale opzione hai scelto: "))
-        except ValueError:
-            print("Devi inserire un numero valido.")
-            continue
-
-        if scelta == 1:
-            # Richiamo della funzione di inserimento
-            nuovi_importi = inserisci_importi()
-            importi_vendita.extend(nuovi_importi)
-            print("Vendite aggiornate:", importi_vendita)
-
-        elif scelta == 2:
-            if importi_vendita:
-                totale_vendite = sum(importi_vendita)
-                media_vendite = totale_vendite / len(importi_vendita)
-                print(f"Totale vendite: {totale_vendite}")
-                print(f"Media vendite: {media_vendite:.2f}")
-            else:
-                print("Non ci sono vendite inserite.")
-
-        elif scelta == 3:
-            print("Uscita dal menu vendite.")
-            break
-
-        else:
-            print("Scelta non valida, riprova.")
-
-
-# Avvio del menu
-menu_vendite()
--- corso python/lunedì_17-11/import_menu_vendite.py
+++ corso python/lunedì_17-11/import_menu_vendite.py
@@ -1,1 +1,41 @@
-from analizzatori_dati_vendita import menu_vendite
+from analizzatori_dati_vendita import inserisci_importi #importa la funzione per l'inserimento degli importi di vendita
+
+
+def menu_vendite():
+    importi_vendita = []  # Lista iniziale vuota che conterrà tutti gli importi inseriti
+
+    while True: #ciclo while del menu che mostra le opzioni fin quando non ne viene scelta una 
+        print("\n--- Menu Vendite ---")
+        print("1. Inserisci nuove vendite")
+        print("2. Mostra totale e media delle vendite")
+        print("3. Esci")
+
+       
+        scelta = int(input("Quale opzione hai scelto: ")) #chiede in input un delle opzioni presenti nel menù
+        
+
+        if scelta == 1:
+            # Richiamo della funzione di inserimento
+            nuovi_importi = inserisci_importi()
+            importi_vendita.extend(nuovi_importi) #aggiunge i nuovi importi alla lista princiapale
+            print("Vendite aggiornate:", importi_vendita)
+
+        elif scelta == 2:
+            if importi_vendita: #mostra totale e media degli importi , solo se esistono.
+                totale_vendite = sum(importi_vendita) #fa la somma degli importi inseriti
+                media_vendite = totale_vendite / len(importi_vendita) #calcola la media delle vendite , dividendo la somma per il numero di elementi nella lista
+                print(f"Totale vendite: {totale_vendite}")            #len(importi_vendita) conta quanti importi sono stati inseriti
+                print(f"Media vendite: {media_vendite:.2f}")
+            else:
+                print("Non ci sono vendite inserite.")
+
+        elif scelta == 3: #esce dal ciclo e termina il menù
+            print("Uscita dal menu vendite.")
+            break
+
+        else: 
+            print("Scelta non valida, riprova.") #stampa se viene scelta un opzione non presente nel menu
+            
+menu_vendite()
+
+
