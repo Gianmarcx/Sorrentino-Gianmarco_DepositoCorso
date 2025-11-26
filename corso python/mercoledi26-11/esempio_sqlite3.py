56d107329a4387412367494dd57b9d8183145271
Gianmarcx <sorrentinogianmarco179@gmail.com>
2025-11-26 15:55:42 +0100

    sqlite3 prima lezione
    sqlite3 prima lezione

--- corso python/mercoledi26-11/esempio_sqlite3.py
+++ corso python/mercoledi26-11/esempio_sqlite3.py
@@ -1,12 +1,9 @@
 import sqlite3
-import os
+
 
 # Nome del file database
 db_filename = 'libreria_esempio.db'
 
-# Rimuovo il file se esiste già per avere un ambiente pulito ad ogni esecuzione
-if os.path.exists(db_filename):
- os.remove(db_filename)
 
 # 1. CREAZIONE E CONNESSIONE
 # Se il file non esiste, viene creato. Se esiste, viene aperto.
@@ -68,3 +65,26 @@ tutti_i_libri = cursor.fetchall()
 for libro in tutti_i_libri:
 # libro è una tupla: (id, titolo, autore, anno, prezzo)
     print(f"ID: {libro[0]} | Titolo: {libro[1]} | Prezzo: {libro[4]}€")
+    
+# 8. LETTURA FILTRATA E AGGIORNAMENTO (UPDATE)
+print("\n--- Aggiornamento Prezzo per Orwell ---")
+# Cerchiamo l'ID di 1984
+cursor.execute("SELECT id FROM libri WHERE autore = ?", ("George Orwell",))
+id_orwell = cursor.fetchone()[0] # fetchone restituisce una singola tupla o None
+
+# Aggiorniamo il prezzo
+nuovo_prezzo = 15.00
+cursor.execute("UPDATE libri SET prezzo = ? WHERE id = ?", (nuovo_prezzo, id_orwell))
+conn.commit()
+print(f"Prezzo aggiornato per ID {id_orwell}.")
+
+# 9. CANCELLAZIONE (DELETE)
+print("\n--- Cancellazione libri vecchi ---")
+cursor.execute("DELETE FROM libri WHERE anno < ?", (1950,))
+print(f"Cancellati {cursor.rowcount} libri pubblicati prima del 1950.")
+conn.commit()
+
+# 10. CHIUSURA CONNESSIONE
+# Rilascia le risorse del file
+conn.close()
+print("\nConnessione chiusa.")
