6f8151b20bc4c828f67526b6bea801b0bdb1e769
Gianmarcx <sorrentinogianmarco179@gmail.com>
2025-12-01 15:41:47 +0100

    lunedi 1 dicembre
    lunedi 1 dicembre

---
+++ corso python/lunedi01-12/machine_learing.py
@@ -0,0 +1,29 @@
+#Per machine learing parliamo del campo di intelligenza artificiale (AI) che permette al pc di imparare dei dati e migliorare le proprie prestazioni senza essere esplicitamente programmati
+#automatizza processi decisionali
+
+#Nel contesto di machine learning un modello è un costrutto matematica che rappresenta la relazione tra input(dati) e output desiderati
+
+#Il machine learing utilizza grandi quantità di dati per insegnare ai modelli di AI
+
+
+#APPRENDIMENTO SUPERVISIONATO: il modello viene addestrato su set di dati che include sia input che gli output desiderati
+#ha come obiettivo costruire un modello che possa fare previsioni accurate su nuovi dati basandosi su questa corrispondenza
+
+#APPRENDIMENTO NON SUPERVISIONATO : Il modello lavora con dati che non hanno etichette
+#l'obiettivo è scoprire strutture nascoste nei dati. Tecniche comuni includono il clustering
+
+#APPRENDIMENTO PER RINFORZO: è una tecnica in cui un agente impara a prendere decisioni ottimzzando 
+
+#OVERFITTIG: si verifica quando un modello impara bene i dettagli dei dati di addestramento, inclusi i rumori e le anomalie , a scapito delle prestrazioni sui nuovi dati
+
+#UNDERFITTING: si verifica quando un modello è troppo semplice per catturare la struttura dei dati 
+
+
+
+#VALIDAZIONE E TEST:
+
+#Dopo l'addestramento i modelli vengono valutati su set di dati, 
+
+
+
+
